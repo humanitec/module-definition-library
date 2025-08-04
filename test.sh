1cfd8ce2ba48efc6373ba3bfb56b34e0170de907
Ben Meier <ben.meier@humanitec.com>
2025-08-04 15:00:09 +0100

    fix: missing semicolon

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 set -eux
 
 export TF_PLUGIN_CACHE_DIR=$(mktemp -d)
-for f in $(find . -name .terraform) do rm -r ${f}; done
-for f in $(find . -name .terraform.lock.hcl) do rm -r ${f}; done
+for f in $(find . -name .terraform); do rm -r ${f}; done
+for f in $(find . -name .terraform.lock.hcl); do rm -r ${f}; done
 for f in `find . -name main.tf`; do pushd .; cd $(dirname $f); tofu init; tofu validate; popd; done
